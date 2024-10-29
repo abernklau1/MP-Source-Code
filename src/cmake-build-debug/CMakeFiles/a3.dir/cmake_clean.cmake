d7264a0a6999f44e3da6d9fb9f4f48ae8abbca25
Mallorie Mackay <malloriem123@gmail.com>
2024-10-29 11:44:28 -0500

    Pretty scene with honse

---
+++ src/cmake-build-debug/CMakeFiles/a3.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/a3.dir/Being.cpp.obj"
+  "CMakeFiles/a3.dir/Being.cpp.obj.d"
+  "CMakeFiles/a3.dir/FreeCam.cpp.obj"
+  "CMakeFiles/a3.dir/FreeCam.cpp.obj.d"
+  "CMakeFiles/a3.dir/MPEngine.cpp.obj"
+  "CMakeFiles/a3.dir/MPEngine.cpp.obj.d"
+  "CMakeFiles/a3.dir/Tav.cpp.obj"
+  "CMakeFiles/a3.dir/Tav.cpp.obj.d"
+  "CMakeFiles/a3.dir/horse.cpp.obj"
+  "CMakeFiles/a3.dir/horse.cpp.obj.d"
+  "CMakeFiles/a3.dir/main.cpp.obj"
+  "CMakeFiles/a3.dir/main.cpp.obj.d"
+  "a3.exe"
+  "a3.exe.manifest"
+  "a3.pdb"
+  "liba3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/a3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
